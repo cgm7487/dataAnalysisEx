5cbfce3328ffea1b986eadf4a48abca3b661d848
cgm7487 <cgm7487@gmail.com>
2016-11-11 00:29:43 +0800

    update code

--- extract_clean_data.py
+++ extract_clean_data.py
@@ -2,10 +2,12 @@ import http.client
 import json
 import ast
 
-DATA_UPPER_BOUND = [2000, 255, 10000, 40, 'str', 'str', 40, 0]
+DATA_UPPER_BOUND = [2000, 255, 10000, 'str', 656, 100, 'str','str', 'str', 40, 100]
+
+NUM_OF_DATA = 250
 
 conn = http.client.HTTPSConnection("obd2cloud.apps.exosite.io")
-conn.request("GET", "/development/device/moredata?device=als7061&limit=120")
+conn.request("GET", "/development/device/moredata?device=als7061&limit="+str(NUM_OF_DATA))
 r1 = conn.getresponse()
 data1 = r1.read()
 
@@ -13,18 +15,18 @@ dataString = data1.decode(encoding='UTF-8')
 jsonObj = json.loads(dataString)
 
 import csv
-with open('cardata.csv', 'w', newline='') as csvfile:
+with open('cardata2.csv', 'w', newline='') as csvfile:
     dataWriter = csv.writer(csvfile, delimiter=',')
 	
     dataHeader = []
-    for i in range(8):
+    for i in range(11):
         dataHeader.append(jsonObj['timeseries']['results'][0]['series'][i]['name'])
     dataWriter.writerow(dataHeader)
     
-    for i in range(120):
+    for i in range(NUM_OF_DATA):
         rowData = []
         needToSaveData = True
-        for j in range(8):
+        for j in range(11):
             if type(DATA_UPPER_BOUND[j]) == type(1):
                 if jsonObj['timeseries']['results'][0]['series'][j]['values'][i][1] == 'NODATA' or float(jsonObj['timeseries']['results'][0]['series'][j]['values'][i][1]) > DATA_UPPER_BOUND[j]:
                     needToSaveData = False
--- ml2.py
+++ ml2.py
@@ -9,75 +9,60 @@ from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 
 dataSetXTrain = []
-#dataSetXTest = []
+dataSetXTest = []
 
 dataSetYTrain = []
-#dataSetYTest = []
+dataSetYTest = []
 
 dataIndex = 0
 
-boundary = 40
+boundary = 202
 
 import csv
 with open('learn_data.csv', newline='') as csvfile:
 	carReader = csv.reader(csvfile, delimiter=',')
 	for row in carReader:
-		#if dataIndex < boundary:
-		dataSetXTrain.append(list(map(float, row[0:3])))
-		dataSetYTrain.append(int(row[3]))
-		#if dataIndex >= boundary:
-			#dataSetXTest.append(list(map(float, row[0:3])))
-			#dataSetYTest.append(float(row[3]))
-		#dataIndex += 1
+		if dataIndex < boundary:
+			dataSetXTrain.append(list(map(float, row[0:3])))
+			dataSetYTrain.append(int(int(row[3])/5))
+		if dataIndex >= boundary:
+			dataSetXTest.append(list(map(float, row[0:3])))
+			dataSetYTest.append(int(int(row[3])/5))
+		dataIndex += 1
 
-clf = svm.SVC()
-
-#clf.fit(dataSetXTrain, dataSetYTrain)
-scores = cross_val_score(clf, X = dataSetXTrain, y = dataSetYTrain)
-print(scores)
-print(scores.mean())
-
-
-clf1 = tree.DecisionTreeClassifier()
+clf1 = tree.DecisionTreeClassifier(criterion='entropy')
 #clf1.fit(dataSetXTrain, dataSetYTrain)
+#yPred = clf1.predict(dataSetXTest)
+#print("decision tree score: {}".format(accuracy_score(dataSetYTest, yPred)))
 scores = cross_val_score(clf1, X = dataSetXTrain, y = dataSetYTrain)
 print(scores)
 print(scores.mean())
 
 #print(mean_squared_error(dataSetYTest, clf1.predict(dataSetXTest)))
 
-clf2 = RandomForestClassifier(n_estimators=10)
-
+clf2 = RandomForestClassifier(n_estimators=20)
 #clf2.fit(dataSetXTrain, dataSetYTrain)
+#yPred = clf2.predict(dataSetXTest)
+#print("random forest score: {}".format(accuracy_score(dataSetYTest, yPred)))
 scores = cross_val_score(clf2, X = dataSetXTrain, y = dataSetYTrain)
 print(scores)
 print(scores.mean())
 
 clf3 = AdaBoostClassifier(n_estimators=100)
-
 #clf3.fit(dataSetXTrain, dataSetYTrain)
+#yPred = clf3.predict(dataSetXTest)
+#print("Adaboost score: {}".format(accuracy_score(dataSetYTest, yPred)))
 scores = cross_val_score(clf3, X = dataSetXTrain, y = dataSetYTrain)
 print(scores)
 print(scores.mean())
 
-clf4 = neighbors.KNeighborsClassifier(15, weights='distance')
-
+clf4 = neighbors.KNeighborsClassifier(30, weights='distance')
+#clf4.fit(dataSetXTrain, dataSetYTrain)
+#yPred = clf4.predict(dataSetXTest)
+#print("K-neighborsClassifier score: {}".format(accuracy_score(dataSetYTest, yPred)))
 scores = cross_val_score(clf4, X = dataSetXTrain, y = dataSetYTrain)
 print(scores)
 print(scores.mean())
-
-gnb = GaussianNB()
-scores = cross_val_score(gnb, X = dataSetXTrain, y = dataSetYTrain)
-print(scores)
-print(scores.mean())
-
-#print(mean_squared_error(dataSetYTest, clf2.predict(dataSetXTest)))
-
-#print(clf.predict(dataSetXTest))
-
-#print("Mean squared error: %.2f"
-#      % np.mean((clf.predict(dataSetXTest) - dataSetYTest) ** 2))
-
-#print('Variance score: %.2f' % clf.score(dataSetXTest, dataSetYTest))
