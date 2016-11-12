d7da094ba6043f63d8bf5f37199be3b34b5fd849
cgm7487 <cgm7487@gmail.com>
2016-11-12 11:35:50 +0800

    add 3d plot

--- ml3.py
+++ ml3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from sklearn.cluster import KMeans
 
@@ -8,26 +9,31 @@ dataSetX = []
 random_state = 170
 
 import csv
-with open('learn_data.csv', newline='') as csvfile:
+with open('learn_data2.csv', newline='') as csvfile:
 	carReader = csv.reader(csvfile, delimiter=',')
 	for row in carReader:
-		dataSetX.append(list(map(float, row[0:4])))
+		dataSetX.append(list(map(float, row[0:3])))
 
 arrayX = np.asarray(dataSetX)
 
-y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(arrayX)
+y_pred = KMeans(n_clusters=2, random_state=random_state).fit_predict(arrayX)
 
-plt.subplot(221)
-plt.scatter(arrayX[:, 0], arrayX[:, 2], c=y_pred)
-plt.title("RPM&Fuel")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-plt.subplot(222)
-plt.scatter(arrayX[:, 1], arrayX[:, 2], c=y_pred)
-plt.title("Pedal&Fuel")
+ax.scatter(arrayX[:, 0], arrayX[:, 1], arrayX[:, 2], c= y_pred)
 
-plt.subplot(223)
-plt.scatter(arrayX[:, 3], arrayX[:, 2], c=y_pred)
-plt.title("Speed&Fuel")
+plt.title('RPM&Fuel&Speed k-mean')
+
+plt.show()
+
+#plt.subplot(221)
+#plt.scatter(arrayX[:, 1], arrayX[:, 0], c=y_pred)
+#plt.title("RPM&Fuel")
+
+#plt.subplot(223)
+#plt.scatter(arrayX[:, 1], arrayX[:, 2], c=y_pred)
+#plt.title("Speed&Fuel")
 
 plt.show()
 
